2922bbb4fabf8c1f18f85dd399ac65878eb7d3c3
Venkata Poreddy <vs.poreddy@gmail.com>
2014-10-09 22:10:08 -0700

    Update getht.js

--- getht.js
+++ getht.js
@@ -11,7 +11,6 @@ function onWindowLoad() {
   chrome.tabs.executeScript(null, {
     file: "script.js"
   }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
     if (chrome.extension.lastError) {
       message.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
     }
